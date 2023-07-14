046f0fb2422c532c4c62d52a1c3690b7eb02be11
Fagner86 <fagnersilva009@gmail.com>
2023-07-13 22:45:14 -0300

    AGENDA

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
+const ics = require('ics');
+
+
 
 const app = express();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+
 
 app.use(cors()); // Adicione essa linha antes das rotas
 app.use(express.json()); // Para analisar JSON no corpo da solicitação
@@ -11,22 +17,61 @@ app.use(express.urlencoded({ extended: true })); // Para analisar dados de formu
 app.post('/send-email', async (req, res) => {
   console.log('Endpoint /send-email');
 
+
+
   try {
     const { email, date, endDate, startTime, endTime } = req.body;
-    console.log(email);
 
-    // Aqui pode usar as datas e horas da reserva para personalizar o conteúdo do email
-    const mensagem = `Sua reserva foi confirmada para o período de 
-    ${date} as ${startTime} até ${endDate} as ${endTime}.`;
+    const startDateArray = date.split('-').map(Number);
+    const endDateArray = endDate.split('-').map(Number);
+    const startTimeArray = startTime.split(':').map(Number);
+    const endTimeArray = endTime.split(':').map(Number);
 
-    const msg = {
+  
+    const event = {
+      start: [...startDateArray, ...startTimeArray],
+      end: [...endDateArray, ...endTimeArray],
+      title: 'Reserva',
+      description: 'Sua reserva foi confirmada.',
+      location: 'Local da reserva',
+      organizer: { email: 'ambitechdi@gmail.com', name: 'Ambitech' },
+      method: 'REQUEST',
+      status: 'CONFIRMED',
+      attendees: [
+        { email, name: 'Nome do Participante', rsvp: true, partstat: 'NEEDS-ACTION' },
+      ],
+    };
+
+    const { error, value } = await ics.createEvent(event);
+
+    if (error) {
+      console.error('Erro ao criar o evento ICS:', error);
+      res.status(500).json({ error: 'Erro ao criar o evento ICS.' });
+      return;
+    }
+    const texMessage = `Sua reserva foi confirmada para o período de 
+    ${date} às ${startTime} até ${endDate} às ${endTime}.`;
+
+
+    const icsContent = value;
+    const message = {
       to: email,
       from: 'ambitechdi@gmail.com',
       subject: 'Confirmação de reserva',
-      text: mensagem,
+      text: texMessage,
+      attachments: [
+        {
+          content: Buffer.from(icsContent).toString('base64'),
+          filename: 'convite.ics',
+          type: 'text/calendar',
+          disposition: 'attachment',
+          contentDisposition: 'inline',
+        },
+        
+      ],
     };
 
-    await sgMail.send(msg);
+    await sgMail.send(message);
 
     console.log('Email de confirmação enviado com sucesso.');
 
@@ -37,7 +82,7 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-
 app.listen(process.env.PORT, () => {
   console.log('Servidor iniciado na porta 4000');
 });
+
